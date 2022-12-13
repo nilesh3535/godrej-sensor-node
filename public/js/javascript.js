fbba05ab709f484402a172bded16df34782807cb
nilesh3535 <74583024+nilesh3535@users.noreply.github.com>
2022-12-13 18:36:34 +0530

    Update javascript.js

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,1 +1,14 @@
 
+// $(document).ready(function () {
+// 	$('#dtHorizontalExample').DataTable({
+// 	"scrollX": true,
+// 	});
+// });
+
+// document.getElementById('idate1').value = new Date().toDateInputValue();
+
+$(document).ready( function() {
+    $('#theDate1').val(new Date().toDateInputValue());
+    
+});​
+
